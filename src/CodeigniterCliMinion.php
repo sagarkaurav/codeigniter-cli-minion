ba2d17fc1728bd0d1e478a0c99e66d9592e06cbe
sagar kaurav <sagar.kaurav@gmail.com>
2017-09-24 21:56:31 +0530

    correcting path tp vendor/autoload.php file.

--- src/CodeigniterCliMinion.php
+++ src/CodeigniterCliMinion.php
@@ -1,6 +1,6 @@
 <?php
 namespace Ccm;
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../../../autoload.php';
 
 use Symfony\Component\Console\Application;
 use Ccm\Commands\GenerateControllerCommand;
